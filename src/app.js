d0bc780f7a77dd1bcc7b6fd2db8eae726733ce7f
Shreyansh Kumar <shreyanshkumar5491@gmail.com>
2026-08-02 23:28:14 +0530

    Basic HTTP routes

--- src/app.js
+++ src/app.js
@@ -7,13 +7,32 @@ const app = express(); // creating an instance of express js
 // })
 // This will send irrespective of path, eg local:3000 and local:3000/getjsds - both ll show hello from server
 
-app.use("/test", (req, res) => {
-  res.send("Hello from test");
-}); // This ll listen only when we local:3000/test, other - can't GET
+// app.use("/test", (req, res) => {
+//   res.send("Hello from test");
+// }); // This ll listen only when we local:3000/test, other - can't GET
 
-app.use("/", (req, res) => {
-  res.send("Hello from home");
-}); 
+// app.use("/", (req, res) => {
+//   res.send("Hello from home");
+// });
+
+// app.use - ll match all the http method API calls
+
+// This ll match only to GET call
+
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Shreyansh", lastName: "Kumar" });
+});
+
+app.post("/user", (req, res) => {
+  console.log("Saved");
+  res.send("Date succesfully saved to DB");
+});
+
+app.delete("/user", (req, res) => {
+  console.log("Deleted");
+  res.send("Date Deleted");
+});
+
 app.listen(3000, () => {
   console.log("Server is running listening on port, 3000");
 }); // listen incoming requests, call back is only called whn out=r server is running
