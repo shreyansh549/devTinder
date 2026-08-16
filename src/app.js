ae699cb421666e27f963faf9becad4dbdffbd6d4
Shreyansh Kumar <shreyanshkumar5491@gmail.com>
2026-08-16 12:20:11 +0530

    dynamic add user signup api

--- src/app.js
+++ src/app.js
@@ -5,21 +5,30 @@ const app = express();
 
 const User = require("./models/user");
 
+app.use(express.json()); // a middleware,- works for all the incoming routes. We have to convert it . Our server not able to read JSON data , we need a middleware to convert into readable format(JS Object)
+
 // POST/ sign up/
 
 app.post("/signup", async (req, res) => {
+  //   const user = new User({
+  //     firstName: "shreyansh",
+  //     lastName: "Kumar",
+  //     emailId: "babablacksheep549@gmail.com",
+  //     password: "abc123",
+  //   });
+  // creating a new instance of user model - hardcode example
 
-  const user = new User({
-    firstName: "shreyansh",
-    lastName: "Kumar",
-    emailId: "babablacksheep549@gmail.com",
-    password: "abc123",
-  }); // creating a new instance of user model
+  // req.body - ll give you undefined without(aap.use(express.json())),  bcz it is in JSON format. We need to convert it to JS, for that, we are using use express json -- middleware
 
+  // creating a new instance of user model
+  const user = new User(req.body);
 
-  await user.save(); // return you a promise, save in DB
-
-  res.send("User added succesfully");
+  try {
+    await user.save(); // return you a promise, save in D/B
+    res.send("User added succesfully");
+  } catch (error) {
+    res.status(400).send("Error saving the users:" + error.message);
+  }
 });
 
 connectDB()
@@ -30,5 +39,5 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.log("DB error ", err); 
+    console.log("DB error ", err);
   });
