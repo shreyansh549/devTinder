a5bdca5ef9211a6b061d3887587656c93b524a72
Shreyansh Kumar <shreyanshkumar5491@gmail.com>
2026-09-04 17:13:05 +0530

    Add dotenv configuration and update .gitignore for environment variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,9 @@
 const mongoose = require("mongoose");
-
+require('dotenv').config();
 
 // this is refererring to a cluster
 const connectDB = async () => {
-    // you rmongo db connection string
-  await mongoose.connect(
-    "REMOVED_MONGO_CREDENTIAL",
-  );
+  // you rmongo db connection string
+  await mongoose.connect(process.env.MONGO_URI);
 };
 module.exports = connectDB;
-
